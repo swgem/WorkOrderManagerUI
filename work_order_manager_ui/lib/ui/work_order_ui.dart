1bf1401bc9a3061deced6493191f9e990c10e10c
Eric <eric_h_y@hotmail.com>
2022-10-26 22:20:27 -0300

    Add WorkOrderUi printing retrieved objects as a vertical list and replace default flutter template for this new one

---
+++ work_order_manager_ui/lib/ui/work_order_ui.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:work_order_manager_ui/models/api_services.dart';
+
+import '../models/work_order.dart';
+
+class WorkOrderUi extends StatefulWidget {
+  const WorkOrderUi({super.key});
+
+  @override
+  State<WorkOrderUi> createState() => _WorkOrderUiState();
+}
+
+class _WorkOrderUiState extends State<WorkOrderUi> {
+  List<WorkOrder>? workOrders;
+
+  _getWorkOrders() {
+    ApiServices.fetchWorkOrder().then((response) {
+      Iterable list = jsonDecode(response.body);
+      var workOrderList = list.map((obj) => WorkOrder.fromObject(obj)).toList();
+      setState(() {
+        workOrders = workOrderList;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getWorkOrders();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Work Order List')),
+        body: (workOrders == null)
+            ? const Center(child: Text("Empty"))
+            : ListView.builder(
+                itemCount: workOrders?.length,
+                itemBuilder: (content, index) => Card(
+                      color: Colors.white,
+                      elevation: 2.0,
+                      child: ListTile(
+                        title: ListTile(
+                          title:
+                              Text(workOrders?[index].client ?? "EMPTY CLIENT"),
+                          subtitle: Text(workOrders?[index].serviceSummary ??
+                              "EMPTY SERVICE SUMMARY"),
+                        ),
+                      ),
+                    )));
+  }
+}
