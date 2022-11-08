ebb8dc7cf0f6ac294069bc54340abf3e3326fceb
Eric <eric_h_y@hotmail.com>
2022-11-08 12:56:02 -0300

    Install adaptive_theme package for dark and light theme better managing
    - Package also saves selected theme mode to SharedPreferences (persists after app reboot)

--- work_order_manager_ui/lib/main.dart
+++ work_order_manager_ui/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:work_order_manager_ui/bloc/work_order_list_bloc.dart';
@@ -17,28 +18,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SEVENCAR ORGANIZADOR',
-      themeMode: ThemeMode.system,
-      theme: ThemeData(
-          colorSchemeSeed: const Color.fromARGB(255, 11, 163, 87),
-          scaffoldBackgroundColor: const Color.fromARGB(255, 226, 226, 226),
-          inputDecorationTheme: const InputDecorationTheme(
-              filled: true, fillColor: Color.fromARGB(255, 255, 255, 255)),
-          expansionTileTheme: const ExpansionTileThemeData(
-              collapsedBackgroundColor: Colors.transparent,
-              backgroundColor: Colors.transparent),
-          dividerColor: Colors.transparent,
-          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap),
-      darkTheme: ThemeData(
-          brightness: Brightness.dark,
-          colorSchemeSeed: const Color.fromARGB(255, 11, 163, 87),
-          expansionTileTheme: const ExpansionTileThemeData(
-              collapsedBackgroundColor: Colors.transparent,
-              backgroundColor: Colors.transparent),
-          dividerColor: Colors.transparent,
-          materialTapTargetSize: MaterialTapTargetSize.shrinkWrap),
-      home: const HomeUi(),
-    );
+    return AdaptiveTheme(
+        light: ThemeData(
+            colorSchemeSeed: const Color.fromARGB(255, 11, 163, 87),
+            scaffoldBackgroundColor: const Color.fromARGB(255, 226, 226, 226),
+            inputDecorationTheme: const InputDecorationTheme(
+                filled: true, fillColor: Color.fromARGB(255, 255, 255, 255)),
+            expansionTileTheme: const ExpansionTileThemeData(
+                collapsedBackgroundColor: Colors.transparent,
+                backgroundColor: Colors.transparent),
+            dividerColor: Colors.transparent,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap),
+        dark: ThemeData(
+            brightness: Brightness.dark,
+            colorSchemeSeed: const Color.fromARGB(255, 11, 163, 87),
+            expansionTileTheme: const ExpansionTileThemeData(
+                collapsedBackgroundColor: Colors.transparent,
+                backgroundColor: Colors.transparent),
+            dividerColor: Colors.transparent,
+            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap),
+        initial: AdaptiveThemeMode.system,
+        builder: (theme, darkTheme) => MaterialApp(
+              title: 'SEVENCAR ORGANIZADOR',
+              theme: theme,
+              darkTheme: darkTheme,
+              home: const HomeUi(),
+            ));
   }
 }
