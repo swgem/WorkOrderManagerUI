670e88557e5e2f777887d8c58611435087055f6a
Eric <eric_h_y@hotmail.com>
2022-11-18 00:43:20 -0300

    Add flutter_localizations package for text language in Date and Time pickers

--- work_order_manager_ui/lib/main.dart
+++ work_order_manager_ui/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:work_order_manager_ui/bloc/work_order_editor_bloc.dart';
 import 'package:work_order_manager_ui/bloc/work_order_list_bloc.dart';
 import 'package:work_order_manager_ui/ui/pages/home_page_ui.dart';
@@ -9,11 +10,17 @@ import 'package:work_order_manager_ui/ui/pages/settings_page_ui.dart';
 import 'package:work_order_manager_ui/ui/pages/work_orders_page_ui.dart';
 
 void main() {
-  runApp(MultiBlocProvider(providers: [
-    BlocProvider<WorkOrderListBloc>(create: (context) => WorkOrderListBloc()),
-    BlocProvider<WorkOrderEditorBloc>(
-        create: (context) => WorkOrderEditorBloc()),
-  ], child: const MyApp()));
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider<WorkOrderListBloc>(
+            create: (context) => WorkOrderListBloc()),
+        BlocProvider<WorkOrderEditorBloc>(
+            create: (context) => WorkOrderEditorBloc()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -50,6 +57,14 @@ class MyApp extends StatelessWidget {
                 Routes.workOrders: (context) => const WorkOrdersPageUi(),
                 Routes.settings: (context) => const SettingsPageUi()
               },
+              localizationsDelegates: const [
+                GlobalMaterialLocalizations.delegate
+              ],
+              supportedLocales: const [
+                Locale('en'),
+                Locale('pt', 'BR'),
+                Locale('de')
+              ],
               initialRoute: Routes.home,
             ));
   }
