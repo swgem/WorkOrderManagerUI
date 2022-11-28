ee12d482f0b44e1c973e3ac2500e099182e5ca65
Eric <eric_h_y@hotmail.com>
2022-11-28 13:42:54 -0300

    Increase http request timeout to 30s

--- work_order_manager_ui/lib/api/api_services.dart
+++ work_order_manager_ui/lib/api/api_services.dart
@@ -1,6 +1,6 @@
 abstract class ApiServices {
   static const String hostname = 'workordermanagerserver.azurewebsites.net';
-  static const int requestTimeoutSeconds = 10;
+  static const int requestTimeoutSeconds = 30;
   static const Map<String, String> headers = {
     'Content-Type': 'application/json; charset=UTF-8',
   };
