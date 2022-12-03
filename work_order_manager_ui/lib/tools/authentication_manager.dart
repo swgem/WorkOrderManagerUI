c40904068c9de6f48fa5ede907f4fcb307fcbba1
Eric <eric_h_y@hotmail.com>
2022-12-02 23:21:48 -0300

    Replace shared_preferences for flutter_secure_storage for storing JWT
    - In Android preferences, sdk compile and min sdk versions changed to support flutter_secure_storage

--- work_order_manager_ui/lib/tools/authentication_manager.dart
+++ work_order_manager_ui/lib/tools/authentication_manager.dart
@@ -1,28 +1,26 @@
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 abstract class AuthenticationManager {
   static const String _prefsJwt = "auth-jwt";
   static const String _prefsExpirationDate = "auth-expirationdate";
 
   static Future setAuth(String? jwt, String? expirationDate) async {
-    var prefs = await SharedPreferences.getInstance();
-    prefs.setString(_prefsJwt, jwt ?? "");
-    prefs.setString(_prefsExpirationDate, expirationDate ?? "");
+    const storage = FlutterSecureStorage();
+    await storage.write(key: _prefsJwt, value: jwt ?? "");
+    await storage.write(key: _prefsExpirationDate, value: expirationDate ?? "");
   }
 
   static Future clearAuth() async {
-    var prefs = await SharedPreferences.getInstance();
-    prefs.setString(_prefsJwt, "");
-    prefs.setString(_prefsExpirationDate, "");
+    await setAuth("", "");
   }
 
   static Future<String> getToken() async {
-    var prefs = await SharedPreferences.getInstance();
-    return prefs.getString(_prefsJwt) ?? "";
+    const storage = FlutterSecureStorage();
+    return await storage.read(key: _prefsJwt) ?? "";
   }
 
   static Future<String> getExpirationDate() async {
-    var prefs = await SharedPreferences.getInstance();
-    return prefs.getString(_prefsExpirationDate) ?? "";
+    const storage = FlutterSecureStorage();
+    return await storage.read(key: _prefsExpirationDate) ?? "";
   }
 }
